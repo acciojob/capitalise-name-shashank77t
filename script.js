1b10deddf6da6a487df859927fecdd252f564ca4
shashank77t <110228304+shashank77t@users.noreply.github.com>
2024-05-17 17:21:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const element=document.querySelector('#fname');
+
+let str="";
+element.addEventListener("input",()=>{
+   if(element.value!=' '){
+       str=element.value;
+   }
+   
+})
+window.addEventListener("click",()=>{
+    element.value=str.toUpperCase();
+})
